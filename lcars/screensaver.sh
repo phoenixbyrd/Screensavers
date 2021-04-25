dbc73b35846870ad18685553c99703d89b79dc44
phoenixbyrd <james.gundlach79@gmail.com>
2021-04-25 09:10:29 -0500

    Fix screensaver.service, was not loading.

--- lcars/screensaver.sh
+++ lcars/screensaver.sh
@@ -6,7 +6,7 @@ IDLE_TIME=$((5*60*1000))
 
 # Sequence to execute when timeout triggers.
 trigger_cmd() {
-    WINEPREFIX="$HOME/.screensavers" wine "$HOME/.screensavers/drive_c/windows/system32/System47.scr" /s
+    WINEPREFIX=/home/pi/.screensavers wine /home/pi/.screensavers/drive_c/windows/system32/System47.scr /s
 }
 
 sleep_time=$IDLE_TIME
--- starfield/install.sh
+++ starfield/install.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 #Setup wine prefix
-dir="$HOME/.screensavers"
+dir=/home/pi/.screensavers
 if [[ ! -e $dir ]]; then
-	WINEARCH=win32 WINEPREFIX="$HOME/.screensavers" winecfg	
+	WINEARCH=win32 WINEPREFIX=/home/pi/.screensavers winecfg	
 fi
 
 #Stop existing screensaver.service
 file=/etc/systemd/system/screensaver.service
 if [[ -f $file ]]; then
 	sudo systemctl stop screensaver	
+	sudo systemctl disable screensaver	
 fi
 
-cp Starfield.scr "$HOME/.screensavers/drive_c/windows/"
-cp screensaver.sh "$HOME/.screensavers/"
+
+cp Starfield.scr /home/pi/.screensavers/drive_c/windows/
+cp screensaver.sh /home/pi/.screensavers/
+chmod +x /home/pi/.screensavers/screensaver.sh
 sudo cp screensaver.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl start screensaver
 sudo systemctl enable screensaver
 
 #Display screensaver after install
-WINEPREFIX="$HOME/.screensavers" wine "$HOME/.screensavers/drive_c/windows/Starfield.scr" /s
+WINEPREFIX=/home/pi/.screensavers wine /home/pi/.screensavers/drive_c/windows/Starfield.scr /s
 
 #Clear screen and print instructions
 clear
